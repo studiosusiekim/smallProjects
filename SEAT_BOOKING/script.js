91d22046e166b3346abb86e264703cb1ada7a89d
Susie Kim <hello@susiekim.de>
2020-02-06 17:13:11 +0100

    populate localstorage to UI

--- SEAT_BOOKING/script.js
+++ SEAT_BOOKING/script.js
@@ -5,12 +5,13 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI()
+
 let ticketPrice = parseInt(movieSelect.value);
 // Save selected movie and price
 function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem('selectedMovieIndex', movieIndex);
   localStorage.setItem('selectedMoviePrice', moviePrice);
-
 }
 
 
@@ -19,15 +20,32 @@ function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-// Set item in 'key:value' pair & in order to save an array in LS we use JSONstringify
+// setItem in 'key:value' pair 
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-    
 
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
   }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
 
 // Movie select element
 movieSelect.addEventListener('change', e => {
@@ -36,6 +54,7 @@ movieSelect.addEventListener('change', e => {
   updateSelectedCount();
 });
 
+
 // Seat click event
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {  
@@ -43,4 +62,7 @@ container.addEventListener('click', e => {
     }
     updateSelectedCount();
 });
+
+// Initial count and total set 
+updateSelectedCount();
 
